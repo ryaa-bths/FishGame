cbb4d9bafe0219ad915266ad74bac301fc9617db
unknown <BT_3W2_01@BKTECH.LOCAL>
2024-11-21 13:29:47 -0500

    Initial commit

---
+++ src/Card.java
@@ -0,0 +1,18 @@
+public class Card{
+
+    String[] suitList = new String[] {"diamonds", "clubs", "hearts", "spades"};
+
+    //suitList = ["diamonds", "clubs", "hearts", "spades"];
+    private String suit = "";
+    private int rank = 0;
+
+
+    public void Card(String suit, int rank){
+        //this.suit
+        //this.rank
+
+    }
+
+    //public
+
+}
